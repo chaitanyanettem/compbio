95cdee2dfa59260b80110403bebd5c7d8550e155
chaitanyanettem <chaitanyanettem@gmail.com>
2013-09-29 03:32:24 -0400

    code

--- comp_bio/large.py
+++ comp_bio/large.py
@@ -2,19 +2,21 @@ import random
 import copy
 import time
 
-conformations = 0 	##Stores the number of conformations found for a particular string.
+n_conf = 0 	##Stores the number of conformations found for a particular string.
 path=[] 			##Stores the path to be followed for a particular conformation.
 save = [] 			##Stores the conformation with the minimum energy.
 min_energy = 1 		##Stores the current minimum energy.
 min_path = [] 
-count = 0 			##Temporary variable used to maintain a count of processed letters 
-					##in input.
 in_string = raw_input("Enter string:")
 in_string = in_string.upper()
 length = len(in_string)
 side = 2*length + 1 
 matrix = [['.' for x in xrange(side)] for x in xrange(side)] #Creates the lattice
 histogram = [0 for x in xrange(20)] #Stores values for histogram.
+conformations = [] ##Saves the current generation of conformations
+count = 0	##Temporary variable used to maintain a count of processed letters 
+			##in input.
+
 
 xpos = ypos = length##xpos and ypos denote the current position in the lattice. Initialized
 					##to the center of the lattice.
@@ -24,5 +26,43 @@ count += 1 							  ##and increments the count by 1. Count is incremented everyt
 									  ##a letter from the string is entered into the lattice.
 directions = 'UDLR'
 
+def mat_dir(s):
+	## This function updates the matrix. It takes two variables as arguments.
+	global matrix, xpos, ypos, count
+	if s=='U':
+		if matrix[ypos-1][xpos] == '.':
+			matrix[ypos-1][xpos] = in_string[count]
+			ypos -= 1
+			return 0
+		return 1
+	elif s=='D':
+		if matrix[ypos+1][xpos] == '.':
+			matrix[ypos+1][xpos] = in_string[count]
+			ypos += 1
+			return 0
+		return 1
+	elif s=='L':
+		if matrix[ypos][xpos-1] == '.':
+			matrix[ypos][xpos-1] = in_string[count]
+			xpos -= 1
+			return 0
+		return 1
+	elif s=='R':
+		if matrix[ypos][xpos+1] == '.':
+			matrix[ypos][xpos+1] = in_string[count]
+			xpos += 1
+			return 0
+		return 1
+
 def initialize(size=1000):
-	global directions, matrix, in_string
+	global matrix, in_string, xpos, ypos
+	for i in xrange(size):
+		
+		matrix[ypos][xpos] = in_string[count]
+		count += 1
+		for i in xrange(1, length):
+			s = random.choice(directions)
+			flag = mat_dir()
+
+
+
